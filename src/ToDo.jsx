572c304eb6b9732f407f3d6a4fd67ec84fce9189
Shubham <1524.shubham@gmail.com>
2022-10-10 10:52:01 +0530

    bug fixing

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -4,7 +4,7 @@ export default function ToDo({ id, task, onChange }) {
 
     return (
         <div className='flex gap-2'>
-            <input type="checkbox" id={id} onChange={onChange} />
+            <input checked={false} type="checkbox" id={id} onChange={onChange} />
             <label htmlFor={id}>{task}</label>
         </div>
     )
